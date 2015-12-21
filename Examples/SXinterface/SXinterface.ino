d4bafd826d9a27601b307a7ea4676f1d7233bb05
gvandersel <gerard.vd.sel@hccnet.ml>
2015-12-21 10:14:03 +0100

    Interface for SXArduino for communication with several commercial programs
    Lets the Arduino function as a serial interface

--- Examples/SXinterface/SXinterface.ino
+++ Examples/SXinterface/SXinterface.ino
@@ -1,10 +1,14 @@
 /*
-  SX interface (66842)
+   SX interface (66824 or 66842)
 
-   This software emulates a SX interface on the ARduino.
+   Creator: Gerard van der Sel
+   Version: 1.0
+   Date: 19-12-2015
+   
+   This software emulates a SX interface on the Arduino.
    Extra hardware needed is de SX-Arduino board which contains the hardware to communicate with the SX-bus.
 
-   Protocol used by the SX-Interface (66842)
+   Protocol used by the SX-Interface (66824 or 66842)
    2 byte message:
    First byte containing command and address:
       Command is located in bit 7 of first byte:
@@ -14,17 +18,21 @@
                127 controls track power
    Second byte containing data (range 0 to 255)
       in case of address 127: 0 trackpower off
-                              1 trackpower on
-      in case of a read value is random and discarded
+                              128 trackpower on
+      in case of a read: value is random and discarded
 
-   Note: For this sketch to run, don't mount switch and LED on de SXArduino board
+   Note: For this sketch to run, don't solder switch and LED on de SXArduino board
 */
-#include <SXArduino.h>
+
+#include "Arduino.h"
+#include "SXArduino.h"
 
 SXArduino SXbus;         // Interface to the SX-bus
-boolean command;         // Received byte is a command
+boolean cmdAdr;          // Received byte is a command
 boolean cmdWrite;        // Write command
-uint_8 address;             // Address for reading or writing
+uint8_t address;         // Address for reading or writing
+uint8_t cmdRcvd;
+uint8_t dtRcvd;
 
 void sxisr(void) {
     // if you want to understand this, see:
@@ -34,49 +42,48 @@ void sxisr(void) {
 
 
 void setup() {
+  // put your setup code here, to run once:
     // initialize serial:
     Serial.begin(19200);
 
     // initialize SX-bus
     SXbus.init();
-    // CHANGE on INT1 triggers the interrupt routine sxisr (see above)
-    attachInterrupt(1, sxisr, CHANGE); 
+    // Rising edge on INT1 triggers the interrupt routine sxisr (see above)
+    attachInterrupt(1, sxisr, RISING); 
 
     // initialize application
-    command = true;
+    cmdAdr = true;
     cmdWrite = false;
 }
 
 void serialEvent() {
-  while (Serial.available()) {
-    // see if there is some data in input buffer
-    if (Serial.available() > 0) {
-        if (command) {
-            // first byte a command
-            uint_8 cmdRcvd = uint_8(Serial.read());
+    // Read all the data
+    while (Serial.available()) {
+        // First byte is a command, decode it
+            cmdRcvd = (uint8_t)Serial.read();
             // If byte value > 127 a write command, data in second byte
             if (cmdRcvd > 127) {
                 cmdWrite = true;
                 address = cmdRcvd - 128;  // address is command - 128
             } else {  // Read command, perform it
                 if (cmdRcvd < 112) {    // Get address data
-                    Serial.print(SXbus.get(cmdRcvd));
+                    Serial.print((char)SXbus.get(cmdRcvd));
                 } else {   // Illegal address, power?
                     if (cmdRcvd == 127) {
-                        Serial.print(SXbus.getPWR());
+                        Serial.print((char)(SXbus.getPWR() * 128));
                     }
                 }
             }
-            command = false;
+            cmdAdr = false;
         } else {
-            // second byte data
-            uint_8 dtRcvd = int(Serial.read());
+            // Second byte data
+            dtRcvd = (uint8_t)Serial.read();
             if (cmdWrite) {
                 if (address < 112) {
                     SXbus.set(address, dtRcvd);
                 } else {
                     if (address == 127) {
-                        if ((dtRcvd & 1) == 0) {
+                        if ((dtRcvd & 128) == 0) {
                             SXbus.setPWR(0);
                         } else {
                             SXbus.setPWR(1);
@@ -85,13 +92,12 @@ void serialEvent() {
                 }
                 cmdWrite = false;
             }
-            command = true;
+            cmdAdr = true;
         }
     }
 }
 
-void Loop() {
-	// Nothing to do.
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Nothing to do.
 }
-  
-

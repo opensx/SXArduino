e64edc8ee9e39f1f360e3301be5ef0041d7b5b03
gvandersel <gerard.vd.sel@hccnet.ml>
2015-12-21 10:11:24 +0100

    Version tested with several control stations
    Tested with:
    Rautenhaus
    Mut
    FCC
    CC2000

--- SXArduino.h
+++ SXArduino.h
@@ -1,28 +1,32 @@
 /*
  * SXArduino.h
  *
- *  Version:    0.5
+ *  Version:    3.0
  *  Copyright:  Gerard van der Sel
  *
+ *  Changed on: 19-12.2015
+ *  Version: 	3.0
+ *  Changes: 	Added some comment. Given its version number.
+ *
  *  Changed on: 30.11.2015
- *  Version: 0.5
- *  Changes: Reading and writing to multiple addresses in one cycle, resolved timing issues.
+ *  Version: 	0.5
+ *  Changes: 	Reading and writing to multiple addresses in one cycle, resolved timing issues.
  *
  *  Changed on: 14.11.2015
- *  Version: 0.4
- *  Changes: Reading and writing to multiple addresses in one cycle.
+ *  Version: 	0.4
+ *  Changes: 	Reading and writing to multiple addresses in one cycle.
   *
  *  Changed on: 27.10.2015
- *  Version: 0.3
- *  Changes: onWait() added to synchronise with the SXbus.
+ *  Version: 	0.3
+ *  Changes: 	onWait() added to synchronise with the SXbus.
  *
  *  Changed on: 27.09.2015
- *  Version: 0.2
- *  Changes: Minor changes 
+ *  Version: 	0.2
+ *  Changes: 	Minor changes 
  *
  *  Changed on: 10.07.2015
- *  Version: 0.1
- *  Changes: Initial version
+ *  Version: 	0.1
+ *  Changes: 	Initial version
  *
  *  interface hardware needed ! see 
 
@@ -55,19 +59,19 @@
 
 // define arduino pins, ports and bits
 // depends on the hardware used.
-#define SX_T0			3
+#define SX_T0			3                      // Clock
 #define SX_T0_DDR		DDRD
 #define SX_T0_PORT		PORTD
 #define SX_T0_PORTPIN 	PORTD3
 #define SX_T0_PINREG	PIND
 
-#define SX_T1			5
+#define SX_T1			5                      // Data read
 #define SX_T1_DDR		DDRD
 #define SX_T1_PORT		PORTD
 #define SX_T1_PORTPIN 	PORTD5
 #define SX_T1_PINREG	PIND
 
-#define SX_D			6
+#define SX_D			6                      // Data write
 #define SX_D_DDR		DDRD
 #define SX_D_PORT		PORTD
 #define SX_D_PORTPIN	PORTD6
@@ -75,10 +79,10 @@
 
 
 // defines for state machine
-#define SYNC	0
-#define PWR     1
-#define ADDR    2
-#define DATA	3
+#define DATA	0            // For performance DATA first
+#define SYNC	1            // (Gives fastest code)
+#define PWR     2
+#define ADDR    3
 
 // defines for Selectrix constants
 #define SX_STOP         3    // 3 "0" bits achter elkaar
@@ -88,23 +92,23 @@
 
 #define SX_ADDRESS_NUMBER 112   // SX channels
 
-#define NO_WRITE 256   // Na data to write
+#define NO_WRITE 256         // No data to write
 
 class SXArduino {
 public:
 	SXArduino();
 	void init(void);	
-	uint8_t get(uint8_t);
+	int get(uint8_t);
 	uint8_t set(uint8_t, uint8_t);
 	uint8_t isSet(uint8_t);
-    	uint8_t getPWR(void);
+    uint8_t getPWR(void);
 	void setPWR(uint8_t);
 	void isr(void);
 	uint8_t inSync(void);
 	
 private:
 	void initVar();
-	uint8_t calcIndex(int adr);
+	uint8_t calcIndex(uint8_t adr);
 
 	uint8_t _sx_numFrame;                 // number frame
 	uint8_t _sx_dataFrameCount;           // frame counting
@@ -112,11 +116,11 @@ private:
 	uint8_t _sx_sepCount;                 // bit counting (seperator)
 	uint8_t _sx_byteCount;                // bit counting (byte)
 	
-	uint8_t _sx_PWRBit;                   // current state of POWER on track
-	uint8_t _sx_newPWRBit;                // command POWER on track
+	uint8_t _sx_PWR;                      // current state of POWER on track
+	uint8_t _sx_newPWR;                   // command POWER on track
 
 	uint8_t _sx_read_data;                // read data
-    	uint8_t _sx_write_data;  			  // data to write
+    uint8_t _sx_write_data;  			  // data to write
 	uint8_t _sx_index;                    // current index in the array
 	uint8_t _sx_writing;				  // active during the actual writing
 	
